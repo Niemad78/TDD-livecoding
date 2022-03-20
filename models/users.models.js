9c884b4ab7095526a0f29049eac70cf69b0bc623
Damien Mauger <mauger.damien@hotmail.fr>
2022-03-20 19:11:03 +0100

    add route and cie for users

--- models/users.models.js
+++ models/users.models.js
@@ -5,6 +5,30 @@ const findMany = () => {
   return connection.promise().query(sql);
 };
 
+const findOne = (userId) => {
+  const sql = "SELECT * FROM users WHERE id = ?";
+  return connection.promise().query(sql, [userId]);
+};
+
+const postOne = (user) => {
+  const sql = "INSERT INTO users SET ?";
+  return connection.promise().query(sql, [user]);
+};
+
+const modifyOne = (user, userId) => {
+  const sql = "UPDATE users SET ? WHERE id = ?";
+  return connection.promise().query(sql, [user, userId]);
+};
+
+const deleteOne = (userId) => {
+  const sql = "DELETE FROM users WHERE id = ?";
+  return connection.promise().query(sql, [userId]);
+};
+
 module.exports = {
   findMany,
+  findOne,
+  postOne,
+  modifyOne,
+  deleteOne,
 };
